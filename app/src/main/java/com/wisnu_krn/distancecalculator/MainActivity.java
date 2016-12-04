1a1191bbf1f4f9d61022cfa35be1a754545b193c
wisnukurniawan <wisnuko13@gmail.com>
2016-12-04 09:48:46 +0700

    adding method calculate

--- app/src/main/java/com/wisnu_krn/distancecalculator/MainActivity.java
+++ app/src/main/java/com/wisnu_krn/distancecalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.wisnu_krn.distancecalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,22 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private Button calculateButton;
     private TextView resultTextView;
 
-    //Punya variabel (x1,y1,x2,y2)
-    double x1;
-    double x2;
-    double y1;
-    double y2;
-
-    //Selanjutnya kita buat variabel dx = (x2 - x1) dan dy = (y2 - y1)
-    double dx = x2 - x1;
-    double dy = y2 - y1;
-
-    //Selanjutnya kita pangkatkan dx kuadrat ama dy kuadrat, hasilnya kita simpen di variabel dsquared
-    double dsquared = dx * dx + dy * dy;
-
-    //Terakhir kita dapat result nya dengan cara mengakarkan nilai dsquared
-    double result = Math.sqrt(dsquared);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +29,42 @@ public class MainActivity extends AppCompatActivity {
         resetButton = (Button) findViewById(R.id.btn_reset);
         calculateButton = (Button) findViewById(R.id.btn_calculate);
         resultTextView = (TextView) findViewById(R.id.tv_result);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: 04/12/2016 calculate
+                //Punya variabel (x1,y1,x2,y2)
+                String x1String = x1EditText.getText().toString().trim();
+                String y1String = y1EditText.getText().toString().trim();
+                String x2String = x2EditText.getText().toString().trim();
+                String y2String = y2EditText.getText().toString().trim();
+
+                double x1 = Double.parseDouble(x1String);
+                double y1 = Double.parseDouble(y1String);
+                double x2 = Double.parseDouble(x2String);
+                double y2 = Double.parseDouble(y2String);
+
+                //Selanjutnya kita buat variabel dx = (x2 - x1) dan dy = (y2 - y1)
+                double dx = x2 - x1;
+                double dy = y2 - y1;
+
+                //Selanjutnya kita pangkatkan dx kuadrat ama dy kuadrat, hasilnya kita simpen di variabel dsquared
+                double dsquared = dx * dx + dy * dy;
+
+                //Terakhir kita dapat result nya dengan cara mengakarkan nilai dsquared
+                double result = Math.sqrt(dsquared);
+
+                resultTextView.setText(result + "");
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: 04/12/2016 reset
+            }
+        });
 
     }
 }
