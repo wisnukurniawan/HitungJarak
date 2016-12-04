bcc0e23c78147408c0796f0eaa231c53905c5301
wisnukurniawan <wisnuko13@gmail.com>
2016-12-04 10:19:12 +0700

    adding error handling

--- app/src/main/java/com/wisnu_krn/distancecalculator/MainActivity.java
+++ app/src/main/java/com/wisnu_krn/distancecalculator/MainActivity.java
@@ -1,11 +1,12 @@
 package com.wisnu_krn.distancecalculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // TODO: 04/12/2016 calculate
                 //Punya variabel (x1,y1,x2,y2)
+                if (!validate()){
+                    Toast.makeText(MainActivity.this, "Input cannot be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 String x1String = x1EditText.getText().toString().trim();
                 String y1String = y1EditText.getText().toString().trim();
                 String x2String = x2EditText.getText().toString().trim();
@@ -56,6 +62,18 @@ public class MainActivity extends AppCompatActivity {
 
         //Terakhir kita dapat result nya dengan cara mengakarkan nilai dsquared
         return (int) Math.sqrt(dsquared);
+    }
+
+    private boolean validate(){
+        if (x1EditText.getText().toString().trim().equals(""))
+            return false;
+        if (y1EditText.getText().toString().trim().equals(""))
+            return false;
+        if (x2EditText.getText().toString().trim().equals(""))
+            return false;
+        if (y2EditText.getText().toString().trim().equals(""))
+            return false;
+        return true;
     }
 
     public void displayResult(int result){
